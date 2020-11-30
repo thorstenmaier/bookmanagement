2231c15c2ee7f6fe0ed323cbd3cc7f5f2cd72096
Thorsten Maier <Thorsten.Maier@oio.de>
2020-11-30 08:41:49 +0100

    Resilience - RetryTemplate - Get master-data in search-service

--- search-service/src/main/java/com/trivadis/SearchController.java
+++ search-service/src/main/java/com/trivadis/SearchController.java
@@ -3,13 +3,16 @@ package com.trivadis;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.retry.RetryCallback;
+import org.springframework.retry.RetryContext;
+import org.springframework.retry.policy.AlwaysRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
@@ -20,9 +23,20 @@ public class SearchController {
     private TreeSet<Book> allBooks = new TreeSet<>();
 
     public SearchController(DiscoveryClient discoveryClient) {
-        List<ServiceInstance> instances = discoveryClient.getInstances("master-data-service");
-        String uri = instances.stream().findFirst().get().getUri().toString();
-        Arrays.stream(new RestTemplate().getForObject(uri + "/book", Book[].class)).forEach(allBooks::add);
+        RetryTemplate retryTemplate = new RetryTemplate();
+        retryTemplate.setRetryPolicy(new AlwaysRetryPolicy());
+        try {
+            Arrays.stream(retryTemplate.execute(new RetryCallback<Book[], Throwable>() {
+                @Override
+                public Book[] doWithRetry(RetryContext context) throws Throwable {
+                    List<ServiceInstance> instances = discoveryClient.getInstances("master-data-service");
+                    String uri = instances.stream().findFirst().get().getUri().toString();
+                    return new RestTemplate().getForObject(uri + "/book", Book[].class);
+                }
+            })).forEach(allBooks::add);
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
     }
 
     @GetMapping("/search")
